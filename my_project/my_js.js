8c08853d93c9c935106b481f5dabf07dcb515d1c
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-01-11 16:49:35 +0300

    Initial commit

---
+++ my_project/my_js.js
@@ -0,0 +1,6 @@
+//jQuery('body').append('<a href="https://google.com">Перейти в гугл!</a>');
+
+jQuery('document').ready(function(){
+    jQuery('body').append('<a href="https://google.com">Перейти в гугл!</a>');
+    jQuery('div').remove();
+});
---
+++ my_project/probe.py
@@ -0,0 +1,5 @@
+string = input().split(' ')
+res = [[]]
+
+for i in string:
+    res.append(i)
