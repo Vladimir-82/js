8a85084b09e9a034d364d85ca6a8105c20157323
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-01-14 15:42:09 +0300

    init

---
+++ my_project/generator.py
@@ -0,0 +1,17 @@
+def my_generator(list_1, list_2):
+    for i in list_1:
+        for j in list_2:
+            yield i, j, i * j
+
+
+lst1 = [3, 4, 5, 6, 7]
+lst2 = [1, 88, 3, 6, 7]
+
+
+gen = my_generator(lst1, lst2)
+for i, j, res in gen:
+    print(i, j, res)
+    if res == 36:
+        print('found')
+        break
+print(next(gen))
--- my_project/probe.py
+++ my_project/probe.py
@@ -1,3 +1,4 @@
+from random import randint
 # n = int(input())
 # [print(*[input() for i in range(m)]) for i in range(n)]
 
@@ -5,21 +6,29 @@
 # matrix = [[int(i) for i in input().split()] for _ in range(n)]
 # print(matrix)
 
-
-def my_generator(list_1, list_2):
-    for i in list_1:
-        for j in list_2:
-            yield i, j, i * j
-
-
-lst1 = [3, 4, 5, 6, 7]
-lst2 = [1, 88, 3, 6, 7]
+# n = int(input())
+# matrix = [[randint(1, 9) for i in range(n)] for _ in range(n)]
+#
+# print(matrix)
 
 
-gen = my_generator(lst1, lst2)
-for i, j, res in gen:
-    print(i, j, res)
-    if res == 36:
-        print('found')
-        break
-print(next(gen))
+# n = int(input())
+# m = int(input())
+# matrix = [print(*[str(i * j).ljust(3) for i in range(m)]) for j in range(n)]
+
+
+n = int(input())
+m = int(input())
+
+matrix = [[int(i) for i in input().split()] for _ in range(n)]
+
+k = int(input())
+v = int(input())
+
+for i in range(n):
+    for j in range(m):
+        if j == k:
+            matrix[i][k], matrix[i][v] = matrix[i][v], matrix[i][k]
+print(matrix)
+
+
