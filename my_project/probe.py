078727fb308b158f73f90d62cd8204f2a5a18c9e
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-01-17 16:54:56 +0300

    3 matrix

--- my_project/probe.py
+++ my_project/probe.py
@@ -16,18 +16,22 @@ from random import randint
 # m = int(input())
 # matrix = [print(*[str(i * j).ljust(3) for i in range(m)]) for j in range(n)]
 
+# n = int(input())
+# res = [[1 if i == j or i == n - j - 1 else 0 for j in range(n)] for i in range(n)]
+
+
+# n = int(input())
+#
+# matrix = [[0] * n for i in range(n)]
+# for i in range(n):
+#     for j in range(n):
+#         if i == j or i + j + 1 == n:
+#             matrix[j][i] = 1
+# for i in matrix:
+#     print(*i)
 
 n = int(input())
-m = int(input())
-
-matrix = [[int(i) for i in input().split()] for _ in range(n)]
-
-k, v = int(input().split()[0]), int(input().split()[1])
-
-for i in range(n):
-    for j in range(m):
-        if j == k:
-            matrix[i][k], matrix[i][v] = matrix[i][v], matrix[i][k]
-print(matrix)
-
+matrix = [[1 if i <= j and i <= n - 1 - j or i >= j and i >= n - 1 - j else 0 for j in range(n)] for i in range(n)]
+for i in matrix:
+    print(*i)
 
