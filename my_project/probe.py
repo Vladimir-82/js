0270bee610dec6a06cc3a0982c566c6193696fbb
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-01-18 16:18:29 +0300

    m

--- my_project/probe.py
+++ my_project/probe.py
@@ -30,8 +30,22 @@ from random import randint
 # for i in matrix:
 #     print(*i)
 
-n = int(input())
-matrix = [[1 if i <= j and i <= n - 1 - j or i >= j and i >= n - 1 - j else 0 for j in range(n)] for i in range(n)]
-for i in matrix:
-    print(*i)
+# n = int(input())
+# matrix = [[1 if i <= j and i <= n - 1 - j or i >= j and i >= n - 1 - j else 0 for j in range(n)] for i in range(n)]
+# for i in matrix:
+#     print(*i)
+
+
+# n, m = map(int, input().split())
+# row = list(range(1, m + 1))
+# for _ in range(n):
+#     print(*row)
+#     x = row.pop(0)
+#     row.append(x)
+
+n, m = map(int, input().split())
+matrix = [[0] * m for i in range(n)]
+print(matrix)
+print(matrix * 2)
+
 
