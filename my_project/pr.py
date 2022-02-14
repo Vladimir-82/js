07a0855bd49fa2034cbedf0edbfb0cfb2f8f328a
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-02-14 17:43:31 +0300

    myself

--- my_project/pr.py
+++ my_project/pr.py
@@ -1,3 +1,51 @@
-print(list(range(1, 4**2+1)))
-allsum = [5,3,7]
-print(sorted(allsum))
+# n = int(input())
+# d = {}
+# for i in range(n):
+#     words = input().split(':')
+#     d.setdefault(words[0].lower(), words[1].strip())
+#
+# m = int(input())
+# for i in range(m):
+#     search = input().lower()
+#     print(d.get(search, 'Не найдено'))
+
+
+
+# n = int(input())
+#
+# d = {}
+# for el in range(n):
+#     word = input().split()
+#     d.setdefault(word[0], word[1])
+#
+# search = input()
+# for k, v in d.items():
+#     if search == k:
+#         print(v)
+#     if search == v:
+#         print(k)
+
+
+n = int(input())
+d = {}
+for el in range(n):
+    word = input().split()
+    d.setdefault(word[0], [*word[1:]])
+
+m = int(input())
+
+for i in range(m):
+    search = input()
+    for k, v in d.items():
+        if search in v:
+            print(k)
+
+
+# 2
+# Германия Берлин Мюнхен Гамбург Дортмунд
+# Нидерланды Амстердам Гаага Роттердам Алкмар
+# 4
+# Амстердам
+# Алкмар
+# Гамбург
+# Гаага
