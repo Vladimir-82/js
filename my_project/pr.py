f4757fbeca46c6ef63d7838be368e07d91725af1
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-02-15 12:44:55 +0300

    dict

--- my_project/pr.py
+++ my_project/pr.py
@@ -120,7 +120,7 @@ student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', '
 student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
 student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
 
-result = [dict(zip(student_ids, dict(zip(student_names, student_grades))))]
+result = [{student_ids[k]: {student_names[k]: student_grades[k]}} for k in range(len(student_ids))]
 print(result)
 
 # [{'S001': {'Camila Rodriguez': 86}}, {'S002': {'Juan Cruz': 98}},...]
