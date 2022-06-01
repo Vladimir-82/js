c7ec9020ab5e64286e334eab2967754e17957630
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-06-01 14:45:24 +0300

    OverflowError

--- my_project/ostrov.py
+++ my_project/ostrov.py
@@ -1,9 +1,13 @@
-def divisible_count(x,y,k):
-    arr = list(filter(lambda x: not x % k, list(range(x, y + 1))))
-    counter = 0
-    for i in arr:
-        counter += 1
-    return counter
-    # return len([i for i in range(x, y + 1) if not i % k])
+from operator import add
+from functools import reduce
+
+
+
+
+def divisible_count(x, y, k):
+    # return len(list(filter(lambda x: not x % k, tuple(range(x, y + 1)))))
+    # return sum([1 for i in range(x, y + 1) if not i % k])
+    # return sum([1 for i in range(x, y + 1) if not i % k])
+    return reduce(add, [1 for i in range(x, y + 1) if not i % k], 0)
 
 print(divisible_count(6, 11, 2))
