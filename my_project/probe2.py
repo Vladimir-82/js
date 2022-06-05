07d3adf5bbaca34c8e0a9d3936d611069cae8a8b
vladimir <grun_gespenst@tut.by>
2022-06-05 22:02:43 +0300

    Sums of Parts

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,3 +1,18 @@
-a = [3,4,5, []]
+def parts_sums(ls):
+    # Sums of Parts
+    if not ls:
+        return [0]
+    res = []
+    ls.reverse()
+    res.append(ls[-1])
+    for i in ls:
+        res.append(i + res[-1])
+    res.reverse()
+    return res
 
-print(len(a))
+
+
+ls = [0, 1, 3, 6, 10]
+print(parts_sums(ls))
+
+# [20, 20, 19, 16, 10, 0]
