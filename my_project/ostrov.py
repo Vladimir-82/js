717675b8a3b1945ad08e22e8818f305924e3f3a0
vladimir <grun_gespenst@tut.by>
2022-06-27 19:51:31 +0300

    Baby shark lyrics generator

--- my_project/ostrov.py
+++ my_project/ostrov.py
@@ -4,7 +4,7 @@ NAMES = ['Baby', 'Mommy', 'Daddy', 'Grandma', 'Grandpa', "Let's go hunt"]
 
 
 def baby_shark_lyrics():
-    print(*map(lambda x: 3 * (x + ' shark,' + 6 * ' doo' + '\n') + x + ' shark!\n', NAMES))
+    print(*map(lambda x: 3 * (x + ' shark,' + 6 * ' doo' + '\n') + x + ' shark!', NAMES))
     print('Run away,…')
 
 
