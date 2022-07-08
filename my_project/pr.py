30e5583adae319498f1476dbaf7b2f1cab0cae0b
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-07-08 14:36:15 +0300

    link

--- my_project/pr.py
+++
@@ -1,7 +0,0 @@
-import uuid
-
-hostnames = ['google.com', 'yandex.ru']
-for name in hostnames:
-    print(name)
-    print('  MD5   :', uuid.uuid3(uuid.NAMESPACE_DNS, name))
-    print('  SHA-1 :', uuid.uuid5(uuid.NAMESPACE_DNS, name))
