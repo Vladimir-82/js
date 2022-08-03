1208b2e7d84b7cfea2009c486aac0c8b030d7f9e
vladimir <grun_gespenst@tut.by>
2022-08-03 21:18:22 +0300

    Baby shark lyrics generator

--- my_project/ostrov.py
+++ my_project/ostrov.py
@@ -1,12 +1,8 @@
-import uuid
-import pyshorteners
-a = 'https://yandex.by/video/preview/?filmId=10086298432826588759&from=tabbar&parent-reqid=1657269212046069-14730982233553625529-sas3-0998-46d-sas-l7-balancer-8080-BAL-4085&text=%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81+%D1%81%D0%BE%D0%BA%D1%80%D0%B0%D1%89%D0%B5%D0%BD%D0%B8%D1%8F+%D0%B4%D0%BB%D0%B8%D0%BD%D0%BD%D1%8B%D1%85+%D1%81%D1%81%D1%8B%D0%BB%D0%BE%D0%BA+python+%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81'
+"Baby shark lyrics generator"
 
-link = pyshorteners.Shortener().tinyurl.short(a)
-print(link)
+NAMES = ['Baby', 'Mommy', 'Daddy', 'Grandma', 'Grandpa', "Let's go hunt"]
+def baby_shark_lyrics():
+    print(*map(lambda x: 3 * (x + ' shark,' + 6 * ' doo' + '\n') + x + ' shark!\n'.strip(), NAMES))
+    print('Run away,…')
 
-
-test = uuid.uuid1()
-code = test.__str__()[:6]
-
-print(''.join(('https://', 'mydomen/', code)))
+baby_shark_lyrics()
