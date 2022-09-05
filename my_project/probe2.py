275813b0e71fb383c978f4883e54cc414f9e8725
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-09-05 08:36:10 +0300

    eng-blr

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,15 +1,1 @@
-def meeting(s):
-    res = []
-    splt = s.split(';')
-    print(splt)
-    for el in splt:
-        res.append((el.split(':')[::-1]))
-    print(res)
-
-    return sorted(res, key=len)
-
-s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
-print(meeting(s))
-
-
-# "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+import nltk
