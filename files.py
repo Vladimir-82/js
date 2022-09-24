11d94df35a129b9547ebbd97094d0eb6dfe52d04
vladimir <grun_gespenst@tut.by>
2022-09-24 11:30:13 +0300

    repetition

--- files.py
+++ files.py
@@ -1,6 +1,21 @@
-def feast(beast, dish):
-    # your code here
-    return beast.endswith() == dish.endwith(dish[-1])
 
 
-print(feast("great blue heron", "garlic naan"))
+import time
+def time_track(func):
+    def surogate(*args):
+        start = time.time()
+        res = func(*args)
+        stop = time.time()
+        print(f'функция отработала - {stop - start} сек')
+        return res
+    return surogate
+
+@time_track
+def counter(*args):
+    res = 1
+    for i in args:
+        res *= i**5000
+    return len(str(res))
+
+
+print(counter(40006565, 60006, 545465, 656444))
