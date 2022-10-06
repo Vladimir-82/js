5494c23f8685bd824d1c2cccca482e600a59fe4a
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-10-06 08:27:17 +0300

    tex-def

--- my_project/ostrov.py
+++ my_project/ostrov.py
@@ -1,12 +1,5 @@
-def sqInRect(lng, wdth):
-    res = []
-    if lng == wdth:
-        return None
-    massive = [lng, wdth]
-    while massive.count(massive[0]) != len(massive):
-        var = sorted(massive)
-        res.append(var[0])
-        massive = [var[0], var[1] - var[0]]
-    return res + res[-1:]
+def fly_by(lamps, drone):
+    return "o" * len(drone) + lamps[len(drone)+1:]
 
-print(sqInRect(37, 14))
+
+print(fly_by( 'xx', 'T'))
