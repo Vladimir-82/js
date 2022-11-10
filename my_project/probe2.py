f7c1f1a2eeec9ab10b5e56222a528918199102dd
vladimir <grun_gespenst@tut.by>
2022-11-10 21:57:40 +0300

    mas

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,24 +1,7 @@
-def key_difference(dc1, dc2):
-    result = {}
-    dc3 = dc1 | dc2
-    for i, j in dc3.items():
-        if i not in dc1.keys() and i in dc2.keys():
-            result.setdefault(i, 'added')
-        elif i in dc1.keys() and not i in dc2.keys():
-            result.setdefault(i, 'deleted')
-        elif i in dc1.keys() and i in dc2.keys() and dc1.get(i) != dc2.get(i):
-            result.setdefault(i, 'changed')
-        elif i in dc1.keys() and i in dc2.keys() and dc1.get(i) == dc2.get(i):
-            result.setdefault(i, 'equal')
-    return result
+n, m = map(int, input().split())
+mas = [input().split() for j in range(n)]
+ln = len(mas[0])
+[print(*i[-1:ln//2:-1] + i[:ln//2+1]) for i in mas]
 
 
-
-
-
-
-
-dict1 = {'one': 'eon', 'two': 'two', 'four': 'True'}
-dict2 = {'two': 'own', 'zero': '4', 'four': 'True'}
-print(key_difference(dict1, dict2))
 
