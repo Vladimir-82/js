ab00a8cdf30667a3f1749c6fb688e71056cb6542
vladimir <grun_gespenst@tut.by>
2022-11-10 22:21:03 +0300

    hz

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,7 +1,6 @@
 n, m = map(int, input().split())
 mas = [input().split() for j in range(n)]
-ln = len(mas[0])
-[print(*i[-1:ln//2:-1] + i[:ln//2+1]) for i in mas]
+[print(*i[-1:n//2-1:-1] + i[:n//2]) for i in mas]
 
 
 
