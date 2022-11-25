d1729ff5b914897bc9010205ee7bac069237bb51
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-11-25 09:44:28 +0300

    Amazon

--- my_project/test_sample.py
+++ my_project/test_sample.py
@@ -1,6 +1,15 @@
-def func(x):
-    return x + 1
+def gen(lst1, lst2):
+    for i in lst1:
+        for j in lst2:
+            yield i, j, i * j
 
 
-def test_answer():
-    assert func(3) == 4
+lst1 = [4, 5, 6, 7]
+lst2 = [8, 9, 10, 11]
+generator = gen(lst1=lst1, lst2=lst2)
+
+for a, b, res in generator:
+    if res == 45:
+        print(a, b, res)
+        break
+print(next(generator))
