5fa1a5fbf981fb8a9e27fd25e394306727e84570
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-01-24 16:44:00 +0300

    re

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,11 +1,8 @@
-from regex import findall as f
+from re import fullmatch
 
+match1 = fullmatch(r'(-)?(\d+)', '2077')
+match2 = fullmatch(r'(-)?(\d+)', '-1337')
 
-s = 'Peter: (313) 555-1234'
-
-# reg = r'(\(\d{3}\)|\d{3}) (0|-)(\d{3}-\d{4})'
-reg = r'(\([2-9]\d{2}\)|[2-9]\d{2})'
-
-result = f(reg, s)
-
-[print(i) for i in result]
+print(match1.group(1))
+print(match1.group(2))
+print(match2.group(1, 2))
