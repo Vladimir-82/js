3c9b56602ec03d6f0c72a7d84afe35fa1ddad035
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-01-25 16:57:10 +0300

    re-2

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,1 +1,8 @@
+from re import search
+import sys
 
+# patt = r'\b(\w+)\1\b'
+# [print(s, end='') for s in sys.stdin if search(patt, s)]
+
+patt = r'\.*(bee)\.*{2,}'
+[print(s, end='') for s in sys.stdin if search(patt, s)]
