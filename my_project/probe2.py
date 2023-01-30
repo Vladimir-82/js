3ac57b18bbe1cfa41e7f9d159b88dbeeb109ddd1
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-01-30 16:48:42 +0300

    re-3

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,8 +1,14 @@
-from re import search
+import re
 import sys
 
-# patt = r'\b(\w+)\1\b'
-# [print(s, end='') for s in sys.stdin if search(patt, s)]
+regex = r'((https*://)*([a-z\.]+\.[a-z]+/?))">(.*[a-zA-Z]+.*)</a>'
+string = '<li class="tier-2 element-3" role="treeitem"><a href="https://wiki.python.org/moin/BeginnersGuide">Beginner&#39;s Guide</a></li><li class="tier-2 element-4" role="treeitem"><a href="https://devguide.python.org/">Developer&#39;s Guide</a></li><li class="tier-2 element-5" role="treeitem"><a href="https://docs.python.org/faq/">FAQ</a></li><li class="tier-2 element-6" role="treeitem"><a href="http://wiki.python.org/moin/Languages">Non-English Docs</a></li><li class="tier-2 element-7" role="treeitem"><a href="http://python.org/dev/peps/">PEP Index</a></li><li class="tier-2 element-8" role="treeitem"><a href="https://wiki.python.org/moin/PythonBooks">Python Books</a></li>'
 
-patt = r'\.*(bee)\.*{2,}'
-[print(s, end='') for s in sys.stdin if search(patt, s)]
+
+result = re.finditer(regex, string)
+
+for i in result:
+    print(', '.join((i.group(1), i.group(4))), re.MULTILINE)
+
+
+
