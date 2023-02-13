bbdbc98c26abb84c4ea8e2ea2b3a3c89838a9293
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-02-13 15:07:45 +0300

    part 2

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,21 +1,2 @@
-import re
-
-def func(poses, string):
-    pos, endpos = int(poses.split()[0]), int(poses.split()[1])
-    regex_obj = re.compile(r'\d+')
-    result = list(regex_obj.findall(string, pos=pos, endpos=endpos))
-    return sum([int(i) for i in result])
-
-
-
-poses = input()
-string = input()
-
-print(func(poses=poses, string=string))
-
-
-
-
-
-
-
+numbers = input().split()
+print(*sorted(set(filter(lambda x: numbers.count(x) > 1, numbers)), key=int))
