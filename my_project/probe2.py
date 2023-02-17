b053d8df8e6057acedb19fb29947d89f9320657b
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-02-17 15:55:10 +0300

    namesake

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,2 +1,24 @@
-numbers = input().split()
-print(*sorted(set(filter(lambda x: numbers.count(x) > 1, numbers)), key=int))
+import re
+
+current_numbers = int(input())
+
+current_members_list = []
+for i in range(current_numbers):
+    name = input().split('@')[0]
+    current_members_list.append(name)
+
+new_numbers = int(input())
+for j in range(new_numbers):
+    new_name = input()
+
+    namesakes = []
+    for name in current_members_list:
+        match = re.search(new_name, name)
+        if match:
+            namesakes.append(name)
+
+        num = 0
+        while True:
+            if f'{name}'
+
+
