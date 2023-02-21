2ab818a88d5ec311da2da849831ddbef16a04ae0
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-02-21 14:18:01 +0300

    namesake!

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -7,18 +7,32 @@ for i in range(current_numbers):
     name = input().split('@')[0]
     current_members_list.append(name)
 
+
 new_numbers = int(input())
 for j in range(new_numbers):
     new_name = input()
 
-    namesakes = []
     for name in current_members_list:
         match = re.search(new_name, name)
         if match:
-            namesakes.append(name)
 
-        num = 0
-        while True:
-            if f'{name}'
+            n = 0
+            while True:
+                if re.fullmatch(new_name, name):
+                    n += 1
+                    new_name = new_name + str(n)
+                else:
+
+                    current_members_list.append(new_name)
+                    print(new_name + '@beegeek.bzz')
+                    break
+
+
+
+
+
+
+
+
 
 
