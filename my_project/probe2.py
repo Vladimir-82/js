c00aa9b3e077a1dd969161b3fa9d2dad1a6927f5
vladimir <grun_gespenst@tut.by>
2023-02-26 20:23:43 +0300

    data

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,41 +1,12 @@
-import re
+from datetime import date
 
-current_numbers = int(input())
-
-current_members_list = []
-for i in range(current_numbers):
-    name = input().split('@')[0]
-    current_members_list.append(name)
+def get_min_max(dates):
+    if not dates:
+        return ()
+    return min(dates), max(dates)
 
 
-new_numbers = int(input())
-for j in range(new_numbers):
-    new_name = input()
-
-    for name in current_members_list:
-        regex = '(' + name + ')' + '(\d*)'
-        print(regex)
-        match = re.match(regex, name)
-        if match:
-            print(match.group(1))
-
-            # while True:
-            #     if re.fullmatch(new_name, name):
-            #         n += 1
-            #         new_name = new_name + str(n)
-            #     else:
-            #
-            #         current_members_list = [new_name] + current_members_list
-            #         print(new_name + '@beegeek.bzz')
-            #         break
-
-
-
-# timyr-guev2@beegeek.bzz
-
-
-
-
+print(get_min_max([]))
 
 
 
