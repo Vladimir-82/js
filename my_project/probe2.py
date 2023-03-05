5b7acc13dd9fb117bfbbef7123dc433fb24f7e0a
vladimir <grun_gespenst@tut.by>
2023-03-05 20:12:22 +0300

    delta

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,13 +1,5 @@
-from datetime import datetime
-
-with open('diary.txt', 'r', encoding='utf-8') as file:
-    context = file.readlines()
-
-    new_content = {}
-    for string in context:
-
-        if string.strip() == '':
-            continue
-        else:
-            print(string.strip())
+from datetime import datetime, timedelta
+current_day = input()
+date1 = datetime.strptime(current_day, '%H:%M:%S')
+print(date1)
 
