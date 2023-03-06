3eafcbeaa031a5ee67c43c457745ab41dcc503f5
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-03-06 16:56:06 +0300

    year

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,5 +1,24 @@
-from datetime import datetime, timedelta
-current_day = input()
-date1 = datetime.strptime(current_day, '%H:%M:%S')
-print(date1)
+from datetime import date, timedelta
+
+
+def num_of_sundays(y):
+    date1 = date(year=y, month=1, day=1)
+    print(date1)
+    sundays = 0
+    while True:
+        if date1.year == y + 1:
+            break
+        if date1.weekday() == 0:
+            sundays += 1
+        date1 = date1 + timedelta(days=1)
+
+    return sundays
+
+
+
+
+
+
+print(num_of_sundays(768))
+
 
