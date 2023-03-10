bfdacf131ce4b8d1851d41e1b7f2115e4ab9e998
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-03-10 15:55:12 +0300

    dates

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,14 +1,22 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime
 
 pattern = '%d.%m.%Y'
-start, stop = datetime.strptime('01.01.0001', pattern), \
-    datetime.strptime('31.12.9999', pattern)
+number = int(input())
+dates = {}
+
+for member in range(number):
+    first_name, second_name, day = input().split()
+    dates[datetime.strptime(day, pattern).toordinal()] = \
+        dates.get(datetime.strptime(day, pattern).toordinal(), 0) + 1
+
+maximum = max([v for k, v in dates.items()])
+[print(datetime.fromordinal(k).strftime(pattern), sep='\n') for k, v in sorted(dates.items()) if v == maximum]
 
 
-counter = {}
-for i in range(start.toordinal(), stop.toordinal()):
-    if date.fromordinal(i).day == 13:
-        d = date.fromordinal(i).isoweekday()
-        counter[d] = counter.get(d, 0) + 1
 
-[print(v) for k, v in sorted(counter.items())]
+
+
+# if len(oldests) == 1:
+#     [print(datetime.fromordinal(v).strftime(pattern), k) for k, v in dates.items() if v == oldest]
+# else:
+#     print(datetime.fromordinal(oldest).strftime(pattern), len(oldests))
