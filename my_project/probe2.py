e5c179edc416020b28f8d3f3a37f4a64f036161b
vladimir <grun_gespenst@tut.by>
2023-03-25 16:18:39 +0300

    csv

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,24 +1,21 @@
 import csv
 
 
-def csv_columns(filename):
+
+with open('data.csv', encoding='utf-8') as file:
+    rows = csv.reader(file)
+    rows = list(rows)
+    del rows[0]
+    domens = [row[2].split('@')[1] for row in rows]
     dc = {}
-    with open(filename, encoding='utf-8') as file:
-        rows = csv.reader(file)
+    for d in (domens):
+        dc[d] = dc.get(d, 0) + 1
+    data = [[key, value] for key, value in sorted(dc.items(), key=lambda x: (x[1], x[0]))]
 
-        print(list(rows)[0])
+with open('domain_usage.csv', 'w', encoding='utf-8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['domain', 'count',])
+    for row in data:
+        writer.writerow(row)
 
-
-        # for row in list(rows)[1:]:
-        #     for num, el in enumerate(row):
-        #         print(num, el)
-
-                # dc.setdefault(list(rows)[0][num], []).append(el)
-                # dc[list(rows)[0][num]] = dc.get(list(rows)[0][num], []).append(el)
-
-
-    return dc
-
-
-print(csv_columns('1.csv'))
 
