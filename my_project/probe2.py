cf5ec97b44140d54a1907d27d3eefa5dddc44e4f
vladimir <grun_gespenst@tut.by>
2023-04-03 20:47:28 +0300

    zip3

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,11 +1,6 @@
-import json
+from zipfile import ZipFile
 
-
-with open('food_services.json') as file1:
-    data1 = json.load(file1)
-    districts = {}
-    for cort in data1:
-        districts.setdefault(cort["TypeObject"], []).append([cort["Name"], cort["SeatsCount"]])
-    for key, val in sorted(districts.items(), key=lambda y: y[0]):
-        mx = max(val, key=lambda x: int(x[1]))
-        print(key, ', '.join((mx[0], str(mx[1]))), sep=': ')
+with ZipFile('workbook.zip') as zip_file:
+    info = zip_file.infolist()
+    print(max(map(lambda x: x.compress_size / x.file_size, info)))
+    # print([x.filename for x in info if  ])
