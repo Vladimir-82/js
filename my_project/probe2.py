7d62232fd695f7dd825e529912f97382bf75fa82
vladimir <grun_gespenst@tut.by>
2023-04-09 22:19:37 +0300

    namedtuple

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,6 +1,10 @@
-from zipfile import ZipFile
+from collections import namedtuple
+import csv
+import datetime
 
-with ZipFile('workbook.zip') as zip_file:
-    info = zip_file.infolist()
-    print(max(map(lambda x: x.compress_size / x.file_size, info)))
-    # print([x.filename for x in info if  ])
+with open('meetings.csv', encoding='utf-8') as file:
+    rows = csv.DictReader(file)
+    for row in sorted(rows, key=lambda x:
+    (datetime.datetime.strptime(x['meeting_date'], '%d.%m.%Y'),
+     datetime.datetime.strptime(x['meeting_time'], '%H:%M').time())):
+        print(f"{row['surname']} {row['name']}")
