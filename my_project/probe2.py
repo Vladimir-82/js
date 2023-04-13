e12e5bb1ff0c4a7f3fa484b8d51ef40c120e75b9
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-04-13 16:47:28 +0300

    counter3

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,15 +1,15 @@
 from collections import Counter
-from string import ascii_lowercase as letters
+
+
+def scrabble(symbols, word):
+    return Counter(symbols) > Counter(word)
 
 
 
-with open('pythonzen.txt', 'r', encoding='utf-8') as file:
-    res = Counter()
-    info = file.readlines()
-    for el in info:
-        res.update(el.lower())
-    keys = filter(lambda x: x in letters, res.keys())
-    for el in sorted(keys):
-        print(f'{el}: {res[el]}')
+
+
+
+
+
 
 
