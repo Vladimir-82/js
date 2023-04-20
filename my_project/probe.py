b51cee4b807a6df3c4ad71663b6e7183b851957c
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-04-20 16:29:10 +0300

    except3

--- my_project/probe.py
+++ my_project/probe.py
@@ -1,2 +1,3 @@
-
-
+s = 'dewewr5'
+d = any((str(i).isdigit() for i in 'dewewr5'))
+print(d)
--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,14 +1,49 @@
-import json
+import sys
 
-try:
-    with open(input()) as file1:
-        data1 = json.load(file1)
-        print(data1)
-except FileNotFoundError:
-    print('Файл не найден')
-except json.decoder.JSONDecodeError:
-    print('Ошибка при десериализации')
 
+class PasswordError(Exception):
+    pass
+
+class LengthError(PasswordError):
+    pass
+
+class LetterError(PasswordError):
+    pass
+
+class DigitError(PasswordError):
+    pass
+
+
+
+def is_good_password(s: str):
+    if len(s) < 9:
+        return "LengthError"
+    if not any(c.islower() for c in s):
+        return "LetterError"
+    if not any(c.isupper() for c in s):
+        return "LetterError"
+    if not any(c.isdigit() for c in s):
+        return "DigitError"
+    return 'Success!'
+
+
+
+
+for password in sys.stdin:
+    print(is_good_password(password))
+    if is_good_password(password) == 'Success!':
+        break
+
+
+# Short7
+# Short71
+# Short712
+# Short7123
+# Beegeek123456
+# Stepik1111111111111
+# Stepik22222222222222
+# Short7
+# Short7
 
 
 
