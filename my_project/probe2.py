98c42f6f122d21055b107ea70ca511c6a4b6656c
vladimir <grun_gespenst@tut.by>
2023-04-25 19:48:18 +0300

    files

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,13 +1,24 @@
-def dict_travel(nested_dicts, new_str=''):
-    for k, v in sorted(nested_dicts.items()):
-        if isinstance(v, dict):
-            dict_travel(v, new_str + f'{k}.')
-        else:
-            print(f'{new_str}{k}: {v}')
+from collections import defaultdict
+
+size_file = {'GB': 1073741824, 'MB': 1048576 , 'KB': 1024, 'B': 1}
+
+with open('files.txt',  encoding='utf-8') as file:
+    files_list = defaultdict(list)
+    files_size = defaultdict(int)
+    files_format = defaultdict(set)
+
+    for f in file.readlines():
+        files_list[f.strip().split()[0].split('.')[-1]].append(f.strip().split()[0])
+        files_size[f.strip().split()[0].split('.')[-1]] += int(f.strip().split()[1]) * size_file[f.strip().split()[2]]
+        files_format[f.strip().split()[0].split('.')[-1]].add(f.strip().split()[2])
+print(files_format)
+for key, val in sorted(files_list.items(), key=lambda x: x[0]):
+    for el in sorted(val):
+        print(el)
+    print('------')
 
 
-data = {'a': 1, 'b': {'c': 30, 'a': 10, 'b': 20}}
-dict_travel(data)
+
 
 
 
