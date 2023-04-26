38b53859a2ceb35c2d74c6c497cdd2ee9bdeb532
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-04-26 16:55:21 +0300

    funcs

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,35 +1,6 @@
-from collections import defaultdict
-
-size_file = {"GB": 1073741824, "MB": 1048576, "KB": 1024, "B": 1}
-
-
-def format_size(size):
-    if size < 1024:
-        return f'Summary: {round(size)} B'
-    size /= 1024
-    if size < 1024:
-        return f'Summary: {round(size)} KB'
-    size /= 1024
-    if size < 1024:
-        return f'Summary: {round(size)} MB'
-    size /= 1024
-    if size < 1024:
-        return f'Summary: {round(size)} GB'
+def convert(n):
+    z = '-' if n < 0 else ''
+    return z + bin(n).split('b')[-1], z + oct(n).split('o')[-1], z + hex(n).split('x')[-1].upper()
 
 
-with open('files.txt',  encoding='utf-8') as file:
-    files_list = defaultdict(list)
-    files_size = defaultdict(int)
-    for f in file.readlines():
-        files_list[f.strip().split()[0].split('.')[-1]].append(f.strip().split()[0])
-        files_size[f.strip().split()[0].split('.')[-1]] += int(f.strip().split()[1]) * size_file[f.strip().split()[2]]
-counter = 0
-for key, val in sorted(files_list.items(), key=lambda x: x[0]):
-
-    for el in sorted(val):
-        print(el)
-    print('-' * 10)
-    print(format_size(files_size[key]))
-    counter += 1
-    if counter < len(files_list):
-        print()
+print(convert(-24))
