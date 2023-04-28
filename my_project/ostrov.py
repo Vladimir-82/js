2ce43f6b1bc022a989286f1ba8ad25654a69c2f3
vladimir <grun_gespenst@tut.by>
2023-04-28 21:32:20 +0300

    func

--- my_project/ostrov.py
+++ my_project/ostrov.py
@@ -1,2 +1,1 @@
-while True:
-    print('hello')
+print(0 * 'e')
--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,14 +1,5 @@
-def zip_longest(*args, fill=None):
-    s = args
-    if not s[0]:
-        return []
-    lenght = len(max(*args, key=len))
-    res = []
-    for el in args:
-        res.append(el + (lenght - len(el)) * [fill])
-    return list(zip(*res))
+import string
+
+s = 'Sorting1234'
 
 
-
-
-print(zip_longest([[]]))
