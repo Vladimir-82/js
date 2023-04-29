fa223f7947a0da0e10973107e37ce6f7de07db34
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-04-29 08:23:18 +0300

    funcs

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,5 +1,7 @@
-import string
+s = input()
 
-s = 'Sorting1234'
-
-
+lower_case = ''.join(filter(lambda x: x.islower(), sorted(s)))
+upper_case = ''.join(filter(lambda x: x.isupper(), sorted(s)))
+odd = ''.join(filter(lambda x: int(x) % 2, filter(lambda y: y.isdigit(), sorted(s))))
+even = ''.join(filter(lambda x: not int(x) % 2, filter(lambda y: y.isdigit(), sorted(s))))
+print(lower_case + upper_case + odd + even)
