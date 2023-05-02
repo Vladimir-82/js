4d45dc29cc77c7d7ebcd728c7de71f79fc26a9e2
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-02 14:09:57 +0300

    fib

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,7 +1,4 @@
-s = input()
+fib = lambda n: 1 if n <= 2 else fib(n - 1) + fib(n - 2)
 
-lower_case = ''.join(filter(lambda x: x.islower(), sorted(s)))
-upper_case = ''.join(filter(lambda x: x.isupper(), sorted(s)))
-odd = ''.join(filter(lambda x: int(x) % 2, filter(lambda y: y.isdigit(), sorted(s))))
-even = ''.join(filter(lambda x: not int(x) % 2, filter(lambda y: y.isdigit(), sorted(s))))
-print(lower_case + upper_case + odd + even)
+
+print(fib(3))
