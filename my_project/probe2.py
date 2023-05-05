c0958eb78bbd01e0d220bdc5b547d0c559842d72
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-05 15:50:59 +0300

    zam2

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,15 +1,25 @@
-from collections import defaultdict
+def sourcetemplate(url):
+    def func(**kwargs):
+        dc = {}
+        res = url
+        if not kwargs:
+            return url
+        else:
+            res += '?'
+            counter = 0
+            for key, val in kwargs.items():
+                dc.setdefault(key, val)
+            for key, val in sorted(dc.items(), key=lambda x: x[0]):
+                res += key + '=' + str(val)
+                counter += 1
+                if counter < len(dc):
+                    res += '&'
+        return res
+    return func
 
+url = 'https://stepik.org/lesson/651459/step/14'
+load = sourcetemplate(url)
+print(load(thread='solutions', unit=648165))
 
-
-def remove_marks(text, marks):
-
-    remove_marks.__dict__.setdefault('count', 0)
-    text = ''.join(filter(lambda x: x not in marks, text))
-    remove_marks.count += 1
-    return text
-
-text = 'Hi! Will we go together?'
-
-print(remove_marks(text, '!?'))
-print(remove_marks.count)
+# https://stepik.org/lesson/651459/step/14?thread=solutions&unit=648165
+# https://stepik.org/lesson/651459/step/14?thread=solutionsunit=648165
