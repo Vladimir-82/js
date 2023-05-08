71a6a51451ba077ee209babc00085a6cd4e31be1
vladimir <grun_gespenst@tut.by>
2023-05-08 21:53:17 +0300

    funcs

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,25 +1,14 @@
-def sourcetemplate(url):
-    def func(**kwargs):
-        dc = {}
-        res = url
-        if not kwargs:
-            return url
-        else:
-            res += '?'
-            counter = 0
-            for key, val in kwargs.items():
-                dc.setdefault(key, val)
-            for key, val in sorted(dc.items(), key=lambda x: x[0]):
-                res += key + '=' + str(val)
-                counter += 1
-                if counter < len(dc):
-                    res += '&'
-        return res
-    return func
+def sort_priority(values, group):
+    first = list(sorted(set(values).intersection(set(group))))
+    second = list(sorted(set(values).difference(set(first))))
+    values[:] = first + second
+    return values
 
-url = 'https://stepik.org/lesson/651459/step/14'
-load = sourcetemplate(url)
-print(load(thread='solutions', unit=648165))
 
-# https://stepik.org/lesson/651459/step/14?thread=solutions&unit=648165
-# https://stepik.org/lesson/651459/step/14?thread=solutionsunit=648165
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {5, 7, 2, 3}
+sort_priority(numbers, group)
+
+print(numbers)
+
+# [2, 3, 5, 7, 1, 4, 6, 8]
