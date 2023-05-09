de30519a5116ccbea66b17d8b74752302cddd9f8
vladimir <grun_gespenst@tut.by>
2023-05-09 18:07:06 +0300

    ann

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,14 +1,5 @@
-def sort_priority(values, group):
-    first = list(sorted(set(values).intersection(set(group))))
-    second = list(sorted(set(values).difference(set(first))))
-    values[:] = first + second
-    return values
-
-
-numbers = [8, 3, 1, 2, 5, 4, 7, 6]
-group = {5, 7, 2, 3}
-sort_priority(numbers, group)
-
-print(numbers)
-
-# [2, 3, 5, 7, 1, 4, 6, 8]
+def cyclic_shift(numbers: list[int], step: int) -> None:
+    if step > 0:
+        numbers[:] = numbers[-step:] + numbers[:-step]
+    else:
+        numbers[:] = numbers[abs(step):] + numbers[:abs(step)]
