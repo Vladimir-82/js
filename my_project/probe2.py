9326fa69e8b515237e5109e60202a8a59e0e5233
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-10 15:26:52 +0300

    decorator

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,5 +1,22 @@
-def cyclic_shift(numbers: list[int], step: int) -> None:
-    if step > 0:
-        numbers[:] = numbers[-step:] + numbers[:-step]
-    else:
-        numbers[:] = numbers[abs(step):] + numbers[:abs(step)]
+def takes_positive(func):
+    def sur(*args, **kwargs):
+        try:
+            res = func(*args, **kwargs)
+            len(arg for arg in args if arg > 0) == len(args)
+            return res
+        except Exception(TypeError) as err1:
+            return err1
+        except Exception(ValueError) as err2:
+            return err2
+    return sur
+
+
+@takes_positive
+def positive_sum(*args):
+    return sum(args)
+
+
+try:
+    print(positive_sum(-3, -2, -1, 0, 1, 2, 3))
+except Exception as err:
+    print(type(err))
