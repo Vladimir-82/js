65c325cbf91c99422eaea8c2e3e3a2737f564239
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-11 16:58:37 +0300

    decorator 2

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,15 +1,12 @@
 def takes_positive(func):
     def sur(*args, **kwargs):
-        try:
-            if not all(isinstance(arg, int) for arg in args):
-                raise ValueError
-            if all(arg > 0 for arg in args):
-                raise TypeError
-            else:
-                res = func(*args, **kwargs)
-                return res
-        except:
-            print('dsws')
+        lst = [*args, *kwargs.values()]
+        if not all(map(lambda x: isinstance(x, int), lst)):
+            raise TypeError
+        elif not all(map(lambda x: x > 0, lst)):
+            raise ValueError
+        res = func(*args, **kwargs)
+        return res
     return sur
 
 
@@ -19,6 +16,6 @@ def positive_sum(*args):
 
 
 try:
-    print(positive_sum(-3, -2, -1, 0, 1, 2, 3))
+    print(positive_sum(4, 2, 5, 4, 1, 2, 3))
 except Exception as err:
     print(type(err))
