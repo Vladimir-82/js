653f35d8fc7ef91779e047e833a7e0b1bdbd3039
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-13 14:51:59 +0300

    part

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,30 +1,7 @@
-from functools import wraps
+from functools import partial
 
 
-def retry(times):
-    def decorator(func):
-        @wraps(func)
-        def surrogate(*args, **kwargs):
-            for i in range(times):
-                try:
-                    func(*args, **kwargs)
-                except Exception:
-                    pass
-                else:
-                    raise Exception('MaxRetriesException')
-            else:
 
-                return func(*args, **kwargs)
-        return surrogate
-    return decorator
+def to_Timur(text):
 
 
-@retry(3)
-def no_way():
-    raise ValueError
-
-
-try:
-    no_way()
-except Exception as e:
-    print(type(e))
