bd3a10954246ade894a8e61cfdff40e50c554164
vladimir <grun_gespenst@tut.by>
2023-05-14 21:58:26 +0300

    partition

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -2,6 +2,6 @@ from functools import partial
 
 
 
-def to_Timur(text):
 
-
+to_Timur = partial(send_email, 'Тимур', 'timyrik20@beegeek.ru')
+send_an_invitation = partial(send_email, text='​Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....')
