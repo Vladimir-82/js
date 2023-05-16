9bf5457ffd3a84a4499986526660cefbb48378f6
vladimir <grun_gespenst@tut.by>
2023-05-16 10:51:19 +0300

    matrix

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,7 +1,13 @@
-from functools import partial
+def transpose(matrix):
+    l = len(matrix[0])
+    s = len(matrix)
+    res = []
+    for row in matrix:
+        for i in range(l):
+            res.append(matrix[s])
+    print(res)
 
+matrix = [[1, 2, 3, 4, 5],
+          [6, 7, 8, 9, 10]]
 
-
-
-to_Timur = partial(send_email, 'Тимур', 'timyrik20@beegeek.ru')
-send_an_invitation = partial(send_email, text='​Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....')
+transpose(matrix)
