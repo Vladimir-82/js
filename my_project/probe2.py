21f7e33b896354a8ee1d3dcdc0730eeb2ed81d8b
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-19 09:43:10 +0300

    iterators

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,18 +1,15 @@
 class Fibonacci:
 
     def __init__(self):
-        self.a = 1
+        self.a = 0
         self.b = 1
-        self.start = 1
 
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start:
-            return self.start
-        del self.start
+
         self.a, self.b = self.b, self.a + self.b
         return self.a
 
