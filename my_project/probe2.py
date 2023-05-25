06e301dcb4a72f43f8728de25cf29f4853fb0b97
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-05-25 13:08:09 +0300

    gen1

--- my_project/probe2.py
+++ my_project/probe2.py
@@ -1,22 +1,20 @@
-from datetime import timedelta, date
+from itertools import cycle
+
+def card_deck(suit):
+    card_values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "валет",
+                   "дама", "король", "туз"]
+    card_mast = ["пик", "треф", "бубен", "червей"]
+    card_mast = cycle([i for i in card_mast if i != suit])
 
 
-def dates(start, count=None):
-    counter = 0
-    while True:
-        yield start
-        counter += 1
-        start += timedelta(days=1)
-        if count is not None:
-            if counter == count:
-                return
-
-generator = dates(date(2022, 3, 8))
-
-print(next(generator))
-print(next(generator))
-print(next(generator))
+    for mast in card_mast:
+        for val in card_values:
+            yield f'{val} {mast}'
 
 
 
 
+generator = card_deck('треф')
+cards = [next(generator) for _ in range(40)]
+
+print(*cards)
